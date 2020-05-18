5d08de1d1cef03e1852c7d11f15150a8c2357e01
Yashkumar Bhensdadiya <patelyashp333@gmail.com>
2020-05-18 10:58:04 +0530

    Creating Intial Coursera Repository

---
+++ stats.h
@@ -0,0 +1,41 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file stats.h 
+ * @brief Basic operations of statistics on unsigned char arry.
+ *
+ * Given an array is of unsigned char type and having fixed length..
+ * we have to perform some basic operations like maximun,minimum,mean,mode,sorting of array etc...
+ *
+ * @author Yashkumar Bhensdadiya
+ * @date 16 May 2020
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* Add Your Declarations and Function Comments here */ 
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+
+
+#endif /* __STATS_H__ */
